475e974936a0af62e91e10bc7dfe392f21bdab37
Sebasian Luna <josebaluna@gmail.com>
2025-04-14 14:05:27 +0200

    Delete en.php

--- en.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_5826da5265b706a0930b039dfdf249c3'] = 'We’ve noticed you are navigating outside of US/Canada , woud you like to be redirected to our International website?';
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_d3d2e617335f08df83599665eef8a418'] = 'Stay on US/Canada';
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_c4408d335012a56ff58937d78050efad'] = 'Yes please take me there!';

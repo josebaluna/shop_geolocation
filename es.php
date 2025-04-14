2c46d3428ca2fcbeb3140236169a761271a2bf06
Sebasian Luna <josebaluna@gmail.com>
2025-04-14 14:05:19 +0200

    Delete es.php

--- es.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_060696171be481bc82d2331fab98ef68'] = 'Hemos detectado que estás navegando desde nuestra página de %1s. ¿Quieres que te redirijamos a %2s?	';
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_27dcc66b29e553f075d95bc4f6f28f4b'] = 'Sí llévame allí!';
-$_MODULE['<{wim_shop_geolocation}prestashop>modalredirect_10818af0b07b1c9ab44d5da7f75786b8'] = 'Permanecer en %1s';
